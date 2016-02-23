c3a538a37a6007aa69953d383ded713b88b14d66
Varun Munjeti <vrunjeti@gmail.com>
2016-02-22 23:28:43 -0600

    load json

--- js/script.js
+++ js/script.js
@@ -1,29 +1,21 @@
-/**
- * Maps all tweets to extract the relevant info
- * @param  {Array} tweets [the list of all tweets]
- * @return {Array}        [a list containing just the relevant information needed]
- */
-var tweetInfo = function(tweets) {
-  var mapped = tweets.map(function(tweet) {
-    return {
-      text: tweet.text,
-      hashtags: tweet.entities.hashtags.map(function(hashtag) { return hashtag.test }),
-      location: tweet.user.location,
-      timestamp: tweet.created_at
-    }
+$(document).ready(function() {
+  var tweetInfooo
+  var filteredByHillary
+  
+  loadJSON('d100', function(data) {
+    tweetInfooo = tweetInfo(data.tweets)
+    filteredByHillary = filterByHashtag(data.tweets, 'Hillary')
+    console.log(tweetInfooo)
+    console.log(filteredByHillary)
+  })
+
+})
+
+var loadJSON = function(dataToLoad, callback) {
+  $.ajax({
+    method: 'GET',
+    url: './json/twitter-data/' + dataToLoad + '.json'
+  }).done(function(res) {
+    callback(res)
   })
 }
-
-/**
- * Filters and returns tweets given a hashtag
- * @param  {Array}  tweets  [the list of all tweets]
- * @param  {String} hashtag [hashtag to filter on]
- * @return {Array}          [filtered tweets]
- */
-var filterByHashtag = function(tweets, hashtag) {
-  return tweets.filter(function(tweet) {
-    // check if the hashtag we're looking for is contained in the tweet's hashtags array
-    return !!~tweet.entities.hashtags.indexOf(hashtag)
-  })
-}
-
