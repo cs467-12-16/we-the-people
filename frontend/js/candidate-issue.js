7596fbec8da898b3167606b001a5c9abd3d740eb
Emily Chao <elchao96@live.com>
2016-02-25 00:03:26 -0600

    pushing minor json change

--- frontend/js/candidate-issue.js
+++ frontend/js/candidate-issue.js
@@ -5,12 +5,12 @@ function drawCandidateIssue(candidate, issue) {
   if (candidate == 'trump' && issue == 'immigration') {
     d3.select('#candidate-zoom').text('Donald Trump');
     d3.select('#issue-zoom').text('Immigration');
-    d3.json('../json/trump-immigration.json', function(error, data) {
+    d3.json('json/trump-immigration.json', function(error, data) {
       data.elements.forEach(function(element, index, array) {
         $('.list-group').append('<li class="list-group-item">' + element + "</li>");
       });
     });
-    d3.json('../json/trump-immigration-sentiment.json', function(error, data) {
+    d3.json('json/trump-immigration-sentiment.json', function(error, data) {
       console.log(data);
       var percentagefromRight = (1 + Math.abs(data.mean)) / 2 * 100;
       console.log(percentagefromRight);
